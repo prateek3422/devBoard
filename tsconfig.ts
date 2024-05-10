8fa2bcdfc9ea4ad352fb8a863087445c984ae615
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-05-10 22:46:46 +0530

    FIRST COMMIT

---
+++ tsconfig.ts
@@ -0,0 +1,16 @@
+{
+    "compilerOptions": {
+      "strict": true,
+      "module": "NodeNext",
+      "moduleResolution": "NodeNext",
+      "noImplicitAny": true,
+      "sourceMap": true,
+      "removeComments": true,
+      // "allowJs": true,
+      "rootDir": "src",
+      "outDir": "dist"
+    },
+    "include": ["src/**/*"],
+    "exclude": ["node_modules"]
+  }
+  
