dd9d814f7b00416afd4df6340caf78bcf889a699
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-06-22 17:34:15 +0530

    blog routes fix

--- src/routes/Blog.Routes.ts
+++ src/routes/Blog.Routes.ts
@@ -1,33 +1,29 @@
-import Router from "express"
-import { jwtVerify, upload } from "../middleware"
-import { createBlog, DeleteBlogs, getAllBlogs, getBlogById, toggleBlog, updateBlog } from "../controllers"
+import Router from "express";
+import { jwtVerify, upload } from "../middleware";
+import {
+  createBlog,
+  DeleteBlogs,
+  getAllBlogs,
+  getBlogById,
+  toggleBlog,
+  updateBlog,
+} from "../controllers";
 
-const router = Router()
+const router = Router();
 
-router.route("/Create-Blog").post(
-    upload.fields([
-        { name: "image", maxCount: 1 },
-    ]),
-    jwtVerify, createBlog)
+router
+  .route("/Create-Blog")
+  .post(upload.fields([{ name: "image", maxCount: 1 }]), jwtVerify, createBlog);
 
-router.route("/getAllBlog").get(
-    jwtVerify, getAllBlogs
-)
-router.route("/getBlogById/:BlogId").get(
-    jwtVerify, getBlogById
-)
+router.route("/getAllBlog").get(getAllBlogs);
 
+router.route("/getBlogById/:BlogId").get(getBlogById);
 
-router.route("/:blogId")
-.patch(
-    upload.single("image"),
-    jwtVerify, updateBlog)
-.delete(jwtVerify, DeleteBlogs)
+router
+  .route("/:blogId")
+  .patch(upload.single("image"), jwtVerify, updateBlog)
+  .delete(jwtVerify, DeleteBlogs);
 
+router.route("/toggle-blog/:blogId").patch(jwtVerify, toggleBlog);
 
-router.route("/toggle-blog/:blogId").patch(jwtVerify, toggleBlog)
-
-
-
-
-export { router as BlogRouter }
+export { router as BlogRouter };
