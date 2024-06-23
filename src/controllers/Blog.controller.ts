3c0fd99462f0a24c5a3e8716ed962305eb444d9e
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-06-23 14:17:53 +0530

    fix blog and questions

--- src/controllers/Blog.controller.ts
+++ src/controllers/Blog.controller.ts
@@ -1,314 +1,355 @@
 import { NextFunction, Request, Response } from "express";
-import { ApiError, ApiResponse, asyncHandler, deleteFromCloudinary, uploadToCloudinary } from "../utils";
-import { createBlogSchema, getAllBlog, updateBlogSchema, } from "../schema";
+import {
+  ApiError,
+  ApiResponse,
+  asyncHandler,
+  deleteFromCloudinary,
+  uploadToCloudinary,
+} from "../utils";
+import { createBlogSchema, getAllBlog, updateBlogSchema } from "../schema";
 import { Blog } from "../models/Blog.models";
 import mongoose from "mongoose";
 
-
-
-const getAllBlogs = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { limit = 10, page = 1, shortBy, shortType, query, userId } = req.query
-
+const getAllBlogs = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      limit = 10,
+      page = 1,
+      shortBy,
+      shortType,
+      query,
+      userId,
+    } = req.query;
 
     const blog = await Blog.aggregate([
-        {
-            $match:
-                query ?
-                    {
-                        title: {
-                            $regex: query,
-                            $options: "i"
-                        }
-                    } : {}
-        },
-
-        {
-            $match: userId ? {
-                //@ts-ignore
-                author: new mongoose.Types.ObjectId(userId)
-            } : {}
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "author",
-                foreignField: "_id",
-                as: "author",
-                pipeline: [
-                    {
-                        $project: {
-                            fullname: 1,
-                            username: 1,
-                            avatar: 1,
-                        }
-                    }
-                ]
+      {
+        $match: query
+          ? {
+              title: {
+                $regex: query,
+                $options: "i",
+              },
             }
-        },
-        {
-            $sort: {
-                [shortBy as string]: shortType === "asc" ? 1 : -1
+          : {},
+      },
+
+      {
+        $match: userId
+          ? {
+              //@ts-ignore
+              author: new mongoose.Types.ObjectId(userId),
             }
+          : {},
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "author",
+          pipeline: [
+            {
+              $project: {
+                fullname: 1,
+                username: 1,
+                avatar: 1,
+              },
+            },
+          ],
         },
+      },
+      {
+        $sort: {
+          [shortBy as string]: shortType === "asc" ? 1 : -1,
+        },
+      },
 
-        {
-            $skip: (parseInt(page as string) - 1) * (parseInt(limit as string))
+      {
+        $skip: (parseInt(page as string) - 1) * parseInt(limit as string),
+      },
+      {
+        $limit: parseInt(limit as string),
+      },
 
-        },
-        {
-            $limit: (parseInt(limit as string))
-        },
-    ])
+      {
+        $lookup: {
+          from: "tags",
+          localField: "tags",
+          foreignField: "_id",
+          as: "tags"
+        }
+      },
+    
+      {
+        $addFields: {
+          tags: {
+            $first: "$tags"
+          }
+        }
+      }
+    ]);
+    console.log(blog)
 
     if (!blog) {
-        return next(new ApiError(400, "blog not found"))
+      return next(new ApiError(400, "blog not found"));
     }
 
-    return res.status(200).json(new ApiResponse(200, blog, "get all blogs"))
-})
-const getBlogById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { BlogId } = req.params
+    return res.status(200).json(new ApiResponse(200, blog, "get all blogs"));
+  }
+);
+const getBlogById = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { BlogId } = req.params;
 
     if (!BlogId) {
-        return next(new ApiError(400, "BlogId is required"))
+      return next(new ApiError(400, "BlogId is required"));
     }
 
     const blog = await Blog.aggregate([
+      {
+        $match: { _id: new mongoose.Types.ObjectId(BlogId) },
+      },
+      {
+        $lookup: {
+          from: "tags",
+          localField: "tags",
+          foreignField: "_id",
+          as: "tags",
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "author",
+        },
+      },
+      {
+        $lookup: {
+          from: "comments",
+          localField: "_id",
+          foreignField: "blog",
+          as: "comments",
+        },
+      },
 
-        {
-            $match: { _id: new mongoose.Types.ObjectId(BlogId) }
+      {
+        $lookup: {
+          from: "likes",
+          localField: "_id",
+          foreignField: "blog",
+          as: "like",
         },
-        {
-            $lookup: {
-                from: "tags",
-                localField: "tags",
-                foreignField: "_id",
-                as: "tags"
-            }
+      },
+      {
+        $addFields: {
+          author: {
+            $first: "$author",
+          },
         },
-        {
-            $lookup: {
-                from: "users",
-                localField: "author",
-                foreignField: "_id",
-                as: "author",
-            }
+      },
+      {
+        $addFields: {
+          tags: {
+            $first: "$tags",
+          },
         },
-        {
-            $lookup: {
-                from: "comments",
-                localField: "_id",
-                foreignField: "blog",
-                as: "comments"
-            }
+      },
+      {
+        $addFields: {
+          like: {
+            $first: "$like",
+          },
         },
-
-        {
-            $lookup: {
-                from: "likes",
-                localField: "_id",
-                foreignField: "blog",
-                as: "like"
-            }
+      },
+      {
+        $addFields: {
+          comments: {
+            $first: "$comments",
+          },
         },
-        {
-            $addFields: {
-                author: {
-                    $first: "$author"
-                },
+      },
+      {
+        $project: {
+          name: 1,
+          title: 1,
+          content: 1,
+          image: 1,
+          isPublic: 1,
+          author: {
+            fullname: 1,
+            username: 1,
+            avatar: {
+              url: 1,
             },
-
+          },
+          tags: 1,
+          like: 1,
+          comments: 1,
+          createdAt: 1,
+          updatedAt: 1,
         },
-        {
-            $addFields: {
-                tags: {
-                    $first: "$tags"
-                }
-            }
-        },
-        {
-            $addFields: {
-                like: {
-                    $first: "$like"
-                }
-            }
-        },
-        {
-            $addFields: {
-                comments: {
-                    $first: "$comments"
-                }
-            }
-        },
-        {
-            $project:{
-                name:1,
-                title:1,
-                content:1,
-                image:1,
-                isPublic:1,
-                author: {
-                    fullname: 1,
-                    username: 1,
-                    avatar: {
-                        url: 1
-                    },
-                },
-                tags: 1,
-                like: 1,
-                comments: 1,
-                createdAt:1,
-                updatedAt:1
-            }
-        }
-
-
-
-    ])
+      },
+    ]);
     if (!blog) {
-        return next(new ApiError(400, "Blog not found"))
+      return next(new ApiError(400, "Blog not found"));
     }
 
-    return res.status(200).json(new ApiResponse(200, blog[0], "Blog by id feched successfully"))
+    return res
+      .status(200)
+      .json(new ApiResponse(200, blog[0], "Blog by id feched successfully"));
+  }
+);
+const createBlog = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, title, content, tags } = createBlogSchema.parse(req.body);
 
-})
-const createBlog = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { name, title, content, tags } = createBlogSchema.parse(req.body)
+    const files = req.files as { [key: string]: Express.Multer.File[] };
 
-    const files = req.files as { [key: string]: Express.Multer.File[] }
-
-    const image = files.image[0]?.path
+    const image = files.image[0]?.path;
 
     if (!image) {
-        return next(new ApiError(400, "image is required"))
+      return next(new ApiError(400, "image is required"));
     }
 
-    const uploadImage = await uploadToCloudinary(image)
+    const uploadImage = await uploadToCloudinary(image);
 
     if (!uploadImage) {
-        return next(new ApiError(400, "image upload failed"))
+      return next(new ApiError(400, "image upload failed"));
     }
 
     const blog = await Blog.create({
-        name,
-        title,
-        content,
-        image: {
-            url: uploadImage.url,
-            public_id: uploadImage.public_id
-        },
-        tags: tags || [],
-        author: req.user?._id,
-    })
+      name,
+      title,
+      content,
+      image: {
+        url: uploadImage.url,
+        public_id: uploadImage.public_id,
+      },
+      tags: tags || [],
+      author: req.user?._id,
+    });
 
     if (!blog) {
-        return next(new ApiError(400, "blog create failed"))
+      return next(new ApiError(400, "blog create failed"));
     }
-    return res.status(201).json(new ApiResponse(201, blog, "blog created successfully"))
-
-
-})
-const updateBlog = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    console.log("hello")
-    const { blogId } = req.params
+    return res
+      .status(201)
+      .json(new ApiResponse(201, blog, "blog created successfully"));
+  }
+);
+const updateBlog = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("hello");
+    const { blogId } = req.params;
     // console.log(blogId)
-    const { name, title, content, } = updateBlogSchema.parse(req.body)
+    const { name, title, content } = updateBlogSchema.parse(req.body);
     if (!blogId) {
-        return next(new ApiError(400, "blogId is required"))
+      return next(new ApiError(400, "blogId is required"));
     }
 
-    const blog = await Blog.findById(blogId)
+    const blog = await Blog.findById(blogId);
 
     //* add image and remove image
     //@ts-ignore
-    deleteFromCloudinary(blog?.image?.public_id)
-    const files = req.files as { [key: string]: Express.Multer.File[] }
-    const image = files.image[0]?.path
+    deleteFromCloudinary(blog?.image?.public_id);
+    const files = req.files as { [key: string]: Express.Multer.File[] };
+    const image = files.image[0]?.path;
 
-    const uploadImage = await uploadToCloudinary(image)
+    const uploadImage = await uploadToCloudinary(image);
 
     if (!uploadImage) {
-        return next(new ApiError(400, "image upload failed"))
+      return next(new ApiError(400, "image upload failed"));
     }
     const updateBlog = await Blog.findByIdAndUpdate(
-        blogId,
-        {
-            $set: {
-                name,
-                title,
-                content,
-                image: {
-                    url: uploadImage.url,
-                    public_id: uploadImage.public_id
-                }
-            }
-        }, {
-        new: true
-    }
-    )
+      blogId,
+      {
+        $set: {
+          name,
+          title,
+          content,
+          image: {
+            url: uploadImage.url,
+            public_id: uploadImage.public_id,
+          },
+        },
+      },
+      {
+        new: true,
+      }
+    );
 
     if (!updateBlog) {
-        return next(new ApiError(400, "blog update failed"))
+      return next(new ApiError(400, "blog update failed"));
     }
 
-    return res.status(200).json(new ApiResponse(200, updateBlog, "blog updated successfully"))
-
-
-})
-const DeleteBlogs = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { blogId } = req.params
+    return res
+      .status(200)
+      .json(new ApiResponse(200, updateBlog, "blog updated successfully"));
+  }
+);
+const DeleteBlogs = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { blogId } = req.params;
 
     if (!blogId) {
-        return next(new ApiError(400, "blogId is required"))
+      return next(new ApiError(400, "blogId is required"));
     }
 
-    const blog = await Blog.findById(blogId)
+    const blog = await Blog.findById(blogId);
     if (!blog) {
-        return next(new ApiError(400, "blog not found"))
+      return next(new ApiError(400, "blog not found"));
     }
     //@ts-ignore
-    deleteFromCloudinary(blog.image?.public_id)
+    deleteFromCloudinary(blog.image?.public_id);
 
-    const deleteBlog = await Blog.findByIdAndDelete(blogId)
+    const deleteBlog = await Blog.findByIdAndDelete(blogId);
 
     if (!deleteBlog) {
-        return next(new ApiError(400, "blog delete failed"))
+      return next(new ApiError(400, "blog delete failed"));
     }
 
-    return res.status(200).json(new ApiResponse(200, {}, "delete blog successfully"))
-})
-const toggleBlog = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { blogId } = req.params
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "delete blog successfully"));
+  }
+);
+const toggleBlog = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { blogId } = req.params;
 
     if (!blogId) {
-        return next(new ApiError(400, "blogId is required"))
+      return next(new ApiError(400, "blogId is required"));
     }
 
-    const blog = await Blog.findById(blogId)
+    const blog = await Blog.findById(blogId);
 
     if (!blog) {
-        return next(new ApiError(400, "blog not found"))
+      return next(new ApiError(400, "blog not found"));
     }
 
     const toggleBlog = await Blog.findByIdAndUpdate(blogId, {
-        $set: {
-            isPublic: !blog.isPublic
-        }
-    })
+      $set: {
+        isPublic: !blog.isPublic,
+      },
+    });
 
     if (!toggleBlog) {
-        return next(new ApiError(400, "toggle blog failed"))
+      return next(new ApiError(400, "toggle blog failed"));
     }
 
-    return res.status(200).json(new ApiResponse(200, toggleBlog, "toggle blog successfully"))
-})
-
+    return res
+      .status(200)
+      .json(new ApiResponse(200, toggleBlog, "toggle blog successfully"));
+  }
+);
 
 export {
-    getAllBlogs,
-    getBlogById,
-    createBlog,
-    updateBlog,
-    DeleteBlogs,
-    toggleBlog
-}
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  DeleteBlogs,
+  toggleBlog,
+};
--- src/controllers/Question.controller.ts
+++ src/controllers/Question.controller.ts
@@ -4,163 +4,249 @@ import { ApiError, ApiResponse, asyncHandler } from "../utils";
 import { Question } from "../models/Question.model";
 import mongoose from "mongoose";
 
-
-const createQuestion = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { title, question, description, tags } = QuestionSchema.parse(req.body)
+const createQuestion = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { title, question, description, tags } = QuestionSchema.parse(
+      req.body
+    );
 
     const questions = await Question.create({
+      title,
+      question,
+      description,
+      tags,
+      owner: req.user?._id,
+    });
+    if (!questions) {
+      return next(new ApiError(400, "question not created"));
+    }
+    return res
+      .status(201)
+      .json(new ApiResponse(201, questions, "question created successfully"));
+  }
+);
+
+const getAllQuestion = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      page = 1,
+      limit = 10,
+      shortBy,
+      shortType,
+      query,
+      userId,
+    } = req.query;
+
+    const questions = await Question.aggregate([
+      {
+        $match: query
+          ? {
+              title: {
+                $regex: query,
+                $options: "i",
+              },
+            }
+          : {},
+      },
+      {
+        $sort: {
+          [shortBy as string]: shortType === "asc" ? 1 : -1,
+        },
+      },
+      {
+        $skip: (parseInt(page as string) - 1) * parseInt(limit as string),
+      },
+      {
+        $limit: parseInt(limit as string),
+      },
+      {
+        $match: userId
+          ? {
+              //@ts-ignore
+              author: new mongoose.Types.ObjectId(userId),
+            }
+          : {},
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+        },
+      },
+
+      {
+        $lookup: {
+          from: "tags",
+          localField: "tags",
+          foreignField: "_id",
+          as: "tags",
+        },
+      },
+      {
+        $addFields: {
+          author: {
+            $first: "$author",
+          },
+        },
+      },
+
+      {
+        $project:{
+            _id: 1,
+            title: 1,
+            question: 1,
+            description: 1,
+            tags: 1,
+            owner: {
+              fullname: 1,
+              username: 1,
+              avatar: {
+                url: 1,
+              },
+            },
+            createdAt: 1,
+            updatedAt: 1,
+        }
+      }
+   
+
+
+  
+    ]);
+
+    if (!questions) {
+      return next(new ApiError(400, "question not found"));
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, questions, "question found successfully"));
+  }
+);
+
+const getQuestionById = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { questionId } = req.params;
+
+    if (!questionId) {
+      return next(new ApiError(400, "question not found"));
+    }
+
+    const question = await Question.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(questionId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+        },
+      },
+      {
+        $lookup: {
+          from: "tags",
+          localField: "tags",
+          foreignField: "_id",
+          as: "tags",
+        },
+      },
+
+      {
+        $lookup: {
+          from: "answers",
+          localField: "_id",
+          foreignField: "question",
+          as: "answer",
+        },
+      },
+
+      {
+        $addFields: {
+          owner: {
+            $first: "$owner",
+          },
+        },
+      },
+
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          question: 1,
+          description: 1,
+          tags: 1,
+          owner: {
+            fullname: 1,
+            username: 1,
+            avatar: {
+              url: 1,
+            },
+          },
+          answer: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+    ]);
+    if (!question) {
+      return next(new ApiError(400, "question not found"));
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, question, "question found successfully"));
+  }
+);
+const updateQuestion = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { questionId } = req.params;
+
+    const { title, question, description, tags } = QuestionSchema.parse(
+      req.body
+    );
+    const questions = await Question.findByIdAndUpdate(
+      questionId,
+      {
         title,
         question,
         description,
         tags,
-        owner: req.user?._id
-    })
+      },
+      {
+        new: true,
+      }
+    );
     if (!questions) {
-        return next(new ApiError(400, "question not created"))
+      return next(new ApiError(400, "question not found"));
     }
-    return res.status(201).json(new ApiResponse(201, questions, "question created successfully"))
-})
+    return res
+      .status(200)
+      .json(new ApiResponse(200, questions, "question updated successfully"));
+  }
+);
 
-const getAllQuestion = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { page = 1, limit = 10, shortBy, shortType, query, userId } = req.query
-
-    const questions = await Question.aggregate([
-        {
-            $match:
-                query ?
-                    {
-                        title: {
-                            $regex: query,
-                            $options: "i"
-                        }
-                    } : {}
-        },
-        {
-            $sort: {
-                [shortBy as string]: shortType === "asc" ? 1 : -1
-            }
-        },
-        {
-            $skip: (parseInt(page as string) - 1) * (parseInt(limit as string))
-
-        },
-        {
-            $limit: (parseInt(limit as string))
-        },
-        {
-            $match: userId ? {
-                //@ts-ignore
-                author: new mongoose.Types.ObjectId(userId)
-            } : {}
-        },
-        {
-
-            $lookup: {
-                from: "users",
-                localField: "author",
-                foreignField: "_id",
-                as: "author"
-            }
-        },
-      
-        {
-            $addFields: {
-                author: {
-                    $first: "$author"
-                }
-            }
-        }
-    ])
-   
-    if (!questions) {
-        return next(new ApiError(400, "question not found"
-
-        ))
-    }
-
-
-    return res.status(200).json(new ApiResponse(200, questions, "question found successfully"))
-
-})
-
-const getQuestionById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { questionId } = req.params
-
-    if (!questionId) {
-        return next(new ApiError(400, "question not found"))
-    }
-
-
-    const question = await Question.aggregate([
-        {
-            $match: {
-                _id: new mongoose.Types.ObjectId(questionId)
-            },
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "owner",
-                foreignField: "_id",
-                as: "owner"
-            },
-        },
-        {
-            $lookup: {
-                from: "tags",
-                localField: "tags",
-                foreignField: "_id",
-                as: "tags"
-            }
-        },
-        {
-            $addFields: {
-                owner: {
-                    $first: "$owner"
-                }
-            }
-        }
-
-    ])
+const deleteQuestion = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { questionId } = req.params;
+    const question = await Question.findByIdAndDelete(questionId);
     if (!question) {
-        return next(new ApiError(400, "question not found"))
+      return next(new ApiError(400, "question not found"));
     }
-    return res.status(200).json(new ApiResponse(200, question, "question found successfully"))
-})
-const updateQuestion = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { questionId } = req.params
-
-    const { title,question, description, tags } = QuestionSchema.parse(req.body)
-    const questions = await Question.findByIdAndUpdate(questionId,{
-        title,
-        question,
-        description,
-        tags
-    }, {
-        new: true
-    })
-    if (!questions) {
-        return next(new ApiError(400, "question not found"))
-    }
-    return res.status(200).json(new ApiResponse(200, questions, "question updated successfully"))
-})
-
-const deleteQuestion = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    const { questionId } = req.params
-    const question = await Question.findByIdAndDelete(questionId)
-    if (!question) {
-        return next(new ApiError(400, "question not found"))
-    }
-    return res.status(200).json(new ApiResponse(200, question, "question deleted successfully"))
-})
-
-
+    return res
+      .status(200)
+      .json(new ApiResponse(200, question, "question deleted successfully"));
+  }
+);
 
 export {
-    createQuestion,
-    getAllQuestion,
-    getQuestionById,
-    updateQuestion,
-    deleteQuestion
-
-}
+  createQuestion,
+  getAllQuestion,
+  getQuestionById,
+  updateQuestion,
+  deleteQuestion,
+};
